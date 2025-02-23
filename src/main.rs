ec070df0b82cab02877ac5591601c64696b2c6d9
xav-ie <github@xav.ie>
2025-02-23 00:20:27 -0500

    chore(axum): init

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,28 @@
+// use axum::handler::HandlerWithoutStateExt;
+use axum::{routing::get, Router};
+
+async fn handler() -> &'static str {
+    "Hello, World!"
+}
+
+// Potentially simpler way to write, but I am following faterthanlime as close
+// as possible for now
+// #[tokio::main]
+// async fn main() {
+//     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+//     axum::serve(listener, handler.into_make_service())
+//         .await
+//         .unwrap();
+//
+//     println!("Hello, world!");
+// }
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let router = Router::new().route("/", get(|| handler()));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let addr = listener.local_addr().unwrap();
+    println!("Listening on {addr}");
+    axum::serve(listener, router).await.unwrap();
 }
