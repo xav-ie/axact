8989f7894025ae0759de518219338b871fd7b38b
xav-ie <github@xav.ie>
2025-02-24 09:16:17 -0500

    feat(/api/realtime_cpus): add websocket endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use axum::{
-    extract::State,
+    extract::{
+        ws::{Message, Utf8Bytes, WebSocket},
+        State, WebSocketUpgrade,
+    },
     http::Response,
     response::{Html, IntoResponse},
     routing::get,
@@ -45,6 +48,7 @@ async fn main() {
         .route("/index.html", get(root_get))
         .route("/index.mjs", get(mjs_get))
         .route("/api/cpus", get(cpus_get))
+        .route("/api/realtime_cpus", get(realtime_cpus_get))
         .with_state(app_state.clone());
 
     tokio::task::spawn_blocking(move || {
@@ -64,4 +68,26 @@ async fn main() {
     let addr = listener.local_addr().unwrap();
     println!("Listening on http://{addr}");
     axum::serve(listener, router).await.unwrap();
+}
+
+#[axum::debug_handler]
+async fn realtime_cpus_get(
+    ws: WebSocketUpgrade,
+    State(state): State<AppState>,
+) -> impl IntoResponse {
+    ws.on_upgrade(|ws: WebSocket| async { realtime_cpus_stream(state, ws).await })
+}
+
+async fn realtime_cpus_stream(app_state: AppState, mut ws: WebSocket) {
+    let mut interval = tokio::time::interval(time::Duration::from_secs(1));
+    loop {
+        interval.tick().await;
+        let payload = {
+            let cpus = app_state.cpus.lock().unwrap();
+            serde_json::to_string(&*cpus).unwrap()
+        };
+        ws.send(Message::Text(Utf8Bytes::from(payload)))
+            .await
+            .unwrap();
+    }
 }
