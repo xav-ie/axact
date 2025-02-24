b36d9f1bb0f4d50409bc2f1a848411ef0cc8207a
xav-ie <github@xav.ie>
2025-02-24 09:29:57 -0500

    feat(index): use realtime_cpus endpoint

--- src/index.mjs
+++ src/index.mjs
@@ -3,9 +3,6 @@ import { h, render } from "https://esm.sh/preact@10.26.2";
 import htm from "https://esm.sh/htm@3.1.1";
 const html = htm.bind(h);
 
-const sleep = (/** @type {number} */ ms) =>
-  new Promise((resolve) => setTimeout(resolve, ms));
-
 const formatNumber = (/** @type {number} */ num) =>
   String(Math.round(num)).padStart(2, "0");
 
@@ -33,18 +30,15 @@ const App = ({ cpus }) => {
   `;
 };
 
-const main = async () => {
-  /** @type {number[]} */
-  const cpuData = await fetch("/api/cpus").then((response) => response.json());
-
+const main = (/** @type {number[]} */ cpuData) => {
   const appElement = document.querySelector("div#app") || document.body;
 
   render(html`<${App} cpus=${cpuData} />`, appElement);
 };
 
-while (true) {
-  await main().catch((error) => {
-    console.error(error);
-  });
-  await sleep(1000);
-}
+const url = new URL("/api/realtime_cpus", location.href);
+url.protocol = url.protocol.replace("http", "ws");
+const ws = new WebSocket(url.href);
+ws.addEventListener("message", (event) => {
+  main(JSON.parse(event.data));
+});
